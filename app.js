6c80a047f858f11a6796d458ffafccc6528f04a4
karlos <carlos.kalifa21@gmail.com>
2024-07-31 23:18:16 -0500

    creacion de router

--- app.js
+++ app.js
@@ -4,12 +4,19 @@ const app = express()
 const port = 3000
 
 /* coneccion a la base de datos */
-const mongoose = require('mongoose');
+/*const mongoose = require('mongoose');
 const usuario = 'full-stack';
 const password = 'wv3fbYIz85Cp6cvU';
 const dbname='mascotas';
 
 const uri = `mongodb+srv://${usuario}:${password}@cluster0.j6obnuu.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`
+*/
+/* conexion a base de datos qya que la otra se pego en mascotas */ 
+const usuario = "full_stack_6pm";
+const password = "VeKkaFOEzuitO0qy";
+const dbName = "veterinaria";
+const uri = `mongodb+srv://${usuario}:${password}@cluster0.zpgqwwo.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
+
 mongoose.connect(uri)
   .then(()=> console.log('conectado a mongodb'))
 .catch(e => console.log('error al conectar',e))
---
+++ router/mascotas.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router}();
+
+const mascota = require('../models/mascota');
+
+router.length("/", async (req, res)=>{
+    try{
+    const arrayMascotas = await Mascota.find();
+    console.log(arrayMascotas)
+} catch (error) {
+    console.log(error)
+}
+
+ //   res.render('mascotas', { arrayMascotas: [
+//     {id: 'mas001', nombre: 'Kira', descripcion: "Perro Pastor Aleman"},
+//        {id: 'mas002', nombre: 'Bella', descripcion: "Gata Criolla"},
+//      {id: 'mas003',
+})
