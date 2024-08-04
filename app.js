26cded885c5e3b998bb792f0624a3a621938b029
karlos <carlos.kalifa21@gmail.com>
2024-08-04 18:14:01 -0500

    creacion de servicios en la carpeta views

--- app.js
+++ app.js
@@ -4,29 +4,34 @@ const app = express()
 const port = 3000
 
 /* coneccion a la base de datos */
-/*const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 const usuario = 'full-stack';
 const password = 'wv3fbYIz85Cp6cvU';
 const dbname='mascotas';
 
 const uri = `mongodb+srv://${usuario}:${password}@cluster0.j6obnuu.mongodb.net/${dbname}?retryWrites=true&w=majority&appName=Cluster0`
-*/
-/* conexion a base de datos qya que la otra se pego en mascotas */ 
+
+/* conexion a base de datos qya que la otra se pego en mascotas 
 const usuario = "full_stack_6pm";
 const password = "VeKkaFOEzuitO0qy";
 const dbName = "veterinaria";
 const uri = `mongodb+srv://${usuario}:${password}@cluster0.zpgqwwo.mongodb.net/${dbName}?retryWrites=true&w=majority&appName=Cluster0`;
-
+*/
 mongoose.connect(uri)
   .then(()=> console.log('conectado a mongodb'))
 .catch(e => console.log('error al conectar',e))
+/* establecee el motor de plantillas */
+app.set('views engine', 'ejs');
+app.set('views', __dirname + '/views');
 
 /* middleware para contenido estatico */
 app.use(express.static(__dirname + '/public'));
 
+
 /* enrutamiento */
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  /*res.send('Hello World!')*/
+  res.render('index', {titulo: "Bienvenido a Node.js con express y con ejs"});  
 })
 
 /* ejemplo de enrutamiento  */
